72ca8b24ce026b2721be8f41aa67a52e560b12c9
eman <eman>
2015-06-07 15:41:01 -0400

    pure prompt, neobundle

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -1,5 +1,5 @@
 source $ZSH/zsh/antigen/antigen.zsh
 
-#antigen bundle sindresorhus/pure
+antigen bundle sindresorhus/pure
 antigen bundle zsh-users/zsh-history-substring-search
 antigen bundle zsh-users/zsh-syntax-highlighting
