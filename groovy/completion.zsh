a4c9503267a1b08f9dc03252d97b49fbf091c240
eman <e@m.an>
2015-08-06 10:53:49 -0700

    gvm

--- groovy/completion.zsh
+++ groovy/completion.zsh
@@ -1,5 +1,7 @@
+export GRADLE_OPTS='-Xms512m -Xmx4G -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp'
+
 function init_gvm {
   source ~/.gvm/bin/gvm-init.sh
 }
+init_gvm
 
-export GRADLE_OPTS='-Xms512m -Xmx4G -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp'
