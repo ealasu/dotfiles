c3d33add90c6da08004db746a0e40839bec7048e
eman <eman>
2016-06-18 19:48:26 -0700

    c

--- bin/deflicker.py
+++ bin/deflicker.py
@@ -5,9 +5,11 @@
 
 from __future__ import division
 import os, sys, re, time, datetime, subprocess, shlex
+from multiprocessing import Pool
 from math import *
 from statistics import median
 from scipy.signal import detrend
+
 
 XMP_TEMPLATE = '''<x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="Magic Lantern">
  <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
@@ -51,6 +53,7 @@ def progress(x, interval=1):
 
 
 def get_median(file):
+    print(file)
     cmd1 = "dcraw -c -d -4 -o 0 '%s'" % file
     cmd2 = "convert - -type Grayscale -scale 500x500 -format %c histogram:info:-"
     p1 = subprocess.Popen(shlex.split(cmd1), stdout=subprocess.PIPE)
@@ -65,34 +68,22 @@ def get_median(file):
             level = int(l[p1+1:p2])
             count = int(l[:p1-2])
             X += [level]*count
-    m = median(X)
+    m = log2(median(X))
     return m
 
-progress("Analyzing RAW exposures...");
-files = sorted((f for f in os.listdir('.') if f.endswith('.CR2')))
-i = 0;
-M = [];
-last = 0
-for k,f in enumerate(files):
-    #print(f)
-    m = get_median(f)
-    #print('  median: %s' % m)
-    ev = log2(m)
-    #print('  ev: %s' % ev)
-    #print('  diff from last: %s' % (ev - last))
-    #last = ev
-    M.append(ev)
 
-    progress(k / len(files))
+if __name__ == '__main__':
+    print("Analyzing RAW exposures...")
+    files = sorted((f for f in os.listdir('.') if f.endswith('.CR2')))
+    with Pool(4) as pool:
+        M = pool.map(get_median, files)
 
-E = [(M[0] - m) for m in M]
-E = detrend(E, type='constant')
+    E = [(M[0] - m) for m in M]
+    E = detrend(E, type='constant')
 
-progress("Writing XMP files...");
-i = 0;
-for k, filename in enumerate(files):
-    ec = E[k]
-    xmp = XMP_TEMPLATE % (ec,)
-    with open(os.path.splitext(filename)[0] + '.xmp', 'w') as f:
-        f.write(xmp)
-    progress(k / len(files))
+    print("Writing XMP files...")
+    for filename, ec in zip(files, E):
+        xmp = XMP_TEMPLATE % (ec,)
+        with open(os.path.splitext(filename)[0] + '.xmp', 'w') as f:
+            f.write(xmp)
+
